dc9091ee41de2050803c5be4ce4807d8137b3c07
Laura Garcia Arch <garch.laura@gmail.com>
2023-10-09 11:50:42 +0200

    cpp00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,8 +6,24 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 10:57:21 by laugarci          #+#    #+#             */
-/*   Updated: 2023/10/09 10:57:22 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/10/09 11:50:02 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 
+int main (int ac, char **av)
+{
+	size_t	i;
+
+	i = 0;
+	if (ac == 2)
+	{
+		while (i < std::strlen(av[1]))
+		{
+			std::cout << (char)toupper(av[1][i]);
+			i++;
+		}
+	}
+	return (0);
+}
