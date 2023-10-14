070dfbf6e5059d22ea00ae1abd55dc4c9d6c5d83
Laura Garcia Arch <garch.laura@gmail.com>
2023-10-15 01:04:14 +0200

    cpp

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -5,9 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/09 10:57:59 by laugarci          #+#    #+#             */
-/*   Updated: 2023/10/09 10:58:00 by laugarci         ###   ########.fr       */
+/*   Created: 2023/10/15 00:38:25 by laugarci          #+#    #+#             */
+/*   Updated: 2023/10/15 01:03:51 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+#include <iostream>
 
+Account::Account(void) //constructor
+{
+
+}
+
+Account::~Account(void) //destructor
+{
+//	std::cout << "index:" <<  _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+}
+
+Account::Account( int initial_deposit )
+{
+}	
+
+void	makeDeposit(int deposit)
+{
+	}
+
+bool makeWithdrawal(int withdrawal)
+{
+}
+
+
+void	Account::_displayTimestamp( void )
+{
+	std::cout << "[19920104_091532] ";
+}
